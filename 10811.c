30a429cc0698e726baf15bc52ba560900421143d
박성현 <sunghyeon5307@gmail.com>
2025-04-05 16:09:43 +0900

    202404030

--- 10811.c
+++ 10811.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-int main(){
+int main() {
     int n,m,a,b;
-    scanf("%d %d",&n,&m);
-    int arr[n];
-    for(int i=1;i<6;i++){
-        arr[i]=i;
+    scanf("%d %d", &n, &m);
+    int arr[n+1];
+    for(int i=0;i<n;i++){
+        arr[i]=i+1;
     }
     for(int i=0;i<m;i++){
-        scanf("%d %d\n",&a,&b);
-        for(int i=a-1;i<b;)
+        scanf("%d %d",&a,&b);
+        while(a<b){
+            int temp=arr[a-1];
+            arr[a-1]=arr[b-1];
+            arr[b-1]=temp;
+            a++;
+            b--;
+        }
     }
-    return 0;
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
 }
